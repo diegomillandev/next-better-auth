a0f67ad2f0ab6dae41c5d011cde49eb365d58bc4
diegomillandev <diegomillandev@gmail.com>
2025-10-23 18:31:52 -0500

    feat: update title in Home component to match branding

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import { Key } from "lucide-react";
+
 export default async function AuthLayout({
   children,
 }: Readonly<{
@@ -5,7 +7,15 @@ export default async function AuthLayout({
 }>) {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">{children}</div>
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <span className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <Key className="w-[18px] h-[18px]" />
+          </div>
+          Better Auth
+        </span>
+        {children}
+      </div>
     </div>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-background">
       <div className="w-full max-w-md space-y-6 p-6">
         <div className="text-center space-y-2">
-          <h1 className="text-3xl font-bold">Better Auth Tutorial</h1>
+          <h1 className="text-3xl font-bold">Better Auth</h1>
           <p className="text-muted-foreground">
             A simple tutorial to get started with Better Auth in your Next.js
             app.
