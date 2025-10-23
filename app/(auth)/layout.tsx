41cc77def8d6d612fd4700a3260d33ed2b98f4b7
diegomillandev <diegomillandev@gmail.com>
2025-10-23 18:32:44 -0500

    feat(auth): replace span with Link component for navigation in AuthLayout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import { Key } from "lucide-react";
+import Link from "next/link";
 
 export default async function AuthLayout({
   children,
@@ -8,12 +9,15 @@ export default async function AuthLayout({
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <span className="flex items-center gap-2 self-center font-medium">
+        <Link
+          href={"/"}
+          className="flex items-center gap-2 self-center font-medium"
+        >
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <Key className="w-[18px] h-[18px]" />
           </div>
           Better Auth
-        </span>
+        </Link>
         {children}
       </div>
     </div>
