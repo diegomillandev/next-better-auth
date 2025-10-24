e1817d798a127d59453a2a3f1b52f0890098b7e0
diegomillandev <diegomillandev@gmail.com>
2025-10-23 20:35:45 -0500

    feat(auth): implement reset password email sending with Resend integration

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "./generated/prisma/client";
 import { nextCookies } from "better-auth/next-js";
+import { getResetPasswordEmailHtml } from "@/components/shared/email-template";
+import { FROM_EMAIL, resend } from "./resend";
 
 const prisma = new PrismaClient();
 
@@ -13,7 +15,31 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: false,
     sendResetPassword: async ({ user, url }) => {
-      console.log(`Send email to ${user.email} with this link: ${url}`);
+      // Send reset password email using Resend
+      try {
+        const emailHtml = getResetPasswordEmailHtml(user.email, url);
+
+        // send the email using Resend
+        const { data, error } = await resend.emails.send({
+          from: FROM_EMAIL,
+          to: "diegomillandev@gmail.com",
+          subject: "Reset Your Password",
+          html: emailHtml,
+        });
+
+        if (error) {
+          console.error("Resend error:", error);
+          throw new Error("Failed to send reset password email");
+        } else {
+          console.log(
+            "Reset password email sent successfully to: ",
+            user.email
+          );
+          console.log("Resend response data:", data?.id);
+        }
+      } catch (error) {
+        console.error("Error sending reset password email:", error);
+      }
     },
   },
   plugins: [nextCookies()],
